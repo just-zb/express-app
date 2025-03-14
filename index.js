b48ab62bece05fb30ae7c32901cc2a2b08edac45
Bao Zhu <ice.michael.zhu@gmail.com>
2025-03-14 20:28:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>you gonna to be all right</h1>')
+  res.send('<h1>We are not strange to love~</h1>')
 })
 
 const PORT = 8080
